0b3bc88c247d12b59d0e50cce30c37af1c405ab0
choigyeongyun <pchlruddb@naver,com>
2017-06-12 17:08:27 +0900

    switch를 클릭 했을 때 이벤트 발생을 위해 onclick을 사용하면 된다

--- app/src/main/java/com/example/choigyeongyun/alarm/Main2Activity.java
+++ app/src/main/java/com/example/choigyeongyun/alarm/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.choigyeongyun.alarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void ok(View v2){
+        finish();
+        Toast toast = Toast.makeText(getApplicationContext(), "알림설정이 완료되었습니다.", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 100);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/choigyeongyun/alarm/MainActivity.java
+++ app/src/main/java/com/example/choigyeongyun/alarm/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.choigyeongyun.alarm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean switchFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     public void btn1(View v){
         Intent intent1 = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(intent1);
+    }
+
+    public void switch1(View v){
+
+    }
+
+    public void switch2(View v){
+
     }
 }
